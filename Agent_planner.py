aa0a026007f775fdacd6cbaeb2bd687f94d51133
Zilun Peng <zilunpeng@gmail.com>
2018-04-19 18:36:54 -0400

    lhs of the equality mat

--- Agent_planner.py
+++ Agent_planner.py
@@ -1,79 +1,47 @@
-import gym
-import gym_lavaland
+from Lavaland_spec import Lavaland_spec as Lavaland
 import numpy as np
-env = gym.make('Simple_training_lavaland-v0')
 
-# h_pos = horizontal position
-# v_pos = vertical position
-def sample_action(action_space, h_pos, v_pos):
-    deleted_action = []
-    if h_pos == 0:
-        deleted_action.append(0)
-    if h_pos == 9:
-        deleted_action.append(1)
-    if v_pos == 0:
-        deleted_action.append(2)
-    if v_pos == 9:
-        deleted_action.append(3)
+class Agent_planner(object):
 
-    action_space = np.delete(action_space, deleted_action)
-    return np.random.choice(action_space, 1)
+    num_rows = 10
+    num_cols = 10
+    num_actions = 4
 
-# w = proxy reward
-# max_step = maximum number of steps agent will take if not reaching the terminal
-# num_traj = number of trajectories that we sample
-# RETURN:
-# phi_trajectories: Phi(Epsilon)
-# path_trajectories: the actual path of each trajectory. A path ends before -1
-def generate_trajectory(w, max_step, num_traj, num_states):
-    phi_trajectories = np.zeros((num_traj,num_states))
-    path_trajectories = np.ones((num_traj,max_step))*-1
-    for eps in range(num_traj):
-        pos = env.reset(w)
-        for step in range(max_step):
-            action = sample_action(np.arange(4), pos[0], pos[1])
-            done, phi_epsilon, pos = env.step(action)
-            path_trajectories[eps, step] = pos
-            if done:
-                break
-                phi_trajectories[eps,:] = phi_epsilon/(step+1) #taking the average so that features are on the same scale
-    return phi_trajectories, path_trajectories
+    def __init__(self, sampled_w):
+        self.linprog_eq_mat = np.zeros((self.num_rows+1, self.num_rows*self.num_cols+1))
+        self.sampled_w = sampled_w
 
-# Calculate the distribution over trajectories (Section 4.1 of the paper)
-def calc_traj_prob(w, trajectories):
-    prob = np.exp(w @ trajectories)
-    prob = prob / np.sum(prob)
-    return prob
+    # Return the linear index given a (cell) position-action pair
+    def pos_action_pair_2_ind(self, row_ind, col_ind, action):
+        linear_idx = (self.num_cols*(col_ind-1) + row_ind-1)*self.num_actions + action
+        return linear_idx
 
-# Calculate expected value of Phi(Epsilon)
-# Phi_trajectories = feature vector of each trajectory
-# traj_prob = probability of the trajectory
-def calc_expected_phi(phi_trajectories, traj_prob):
-    expected_phi = np.multiply(phi_trajectories, traj_prob)
-    return sum(expected_phi)
+    def pos_2_ind(self, row_ind, col_ind):
+        linear_idx = self.num_cols*(col_ind-1) + row_ind
+        return linear_idx
 
-#Note that this is not exactly same as what's in the paper (Equation 3)
-def calc_Z_approx_bayes(num_proxy_rewards, phi_trajectories, W, beta):
-    Z = 0
-    for i in range(num_proxy_rewards):
-        w = W[i,:]
-        traj_prob_dist = calc_traj_prob(w.reshape((1,num_states)), phi_trajectories.reshape((num_states,num_traj)))
-        expected_phi = calc_expected_phi(phi_trajectories, traj_prob_dist)
-        Z = Z + np.exp(beta*np.dot(w, expected_phi))
-    return Z
+    # def form_ineq_mat(self):
+
+    # Form the equality matrix (|S|+1 * |S||A|+1) to the linear programming solver
+    def form_eq_mat(self, gamma):
+        for r_ind in range(1,self.num_rows+1):
+            for c_ind in range(1,self.num_cols+1):
+                p_idx = self.pos_2_ind(r_ind, c_ind)
+                available_actions = Lavaland.get_available_action(r_ind, c_ind)
+                for a_ind in range(len(available_actions)):
+                    pa_idx = self.pos_action_pair_2_ind(r_ind, c_ind, available_actions[a_ind])
+                    self.linprog_eq_mat[p_idx][pa_idx] = 1
+
+                ngbr_pos_coords = Lavaland.get_ngbr_pos_coord(r_ind, c_ind)
+                for n_ind in range(len(ngbr_pos_coords)):
+                    ngbr_pos_coord = ngbr_pos_coords[n_ind]
+                    ngbr_row_idx = ngbr_pos_coord[0]
+                    ngbr_col_idx = ngbr_pos_coord[1]
+                    available_actions = Lavaland.get_available_action(ngbr_row_idx, ngbr_col_idx)
+                    for a_ind in range(len(available_actions)):
+                        pa_idx = self.pos_action_pair_2_ind(ngbr_row_idx, ngbr_col_idx, available_actions[a_ind])
+                        self.linprog_eq_mat[p_idx][pa_idx] = -gamma
+
 
 if __name__ == "__main__":
-    num_states = 4
-    max_step = 100
-    num_traj = 1000
-    num_proxy_rewards = 500
-    beta = 1
-    phi_trajectories, path_trajectories = generate_trajectory(np.array([1,1,1,1]), max_step, num_traj, num_states)
-    W = np.random.uniform(-10,10,(num_proxy_rewards, num_states))
-    Z = calc_Z_approx_bayes(num_proxy_rewards, phi_trajectories, W, beta)
-
-    #Calculate the posterior
-    num_test_proxy_rewards = 100
-    W_test = np.random.uniform(-10,10,(num_test_proxy_rewards,num_states))
-    for i in range(num_proxy_rewards):
-        w = W[i,:]
+    Agent_planner(np.array((1,1))).form_eq_mat(np.array((1,1)))
--- Lavaland_spec.py
+++ Lavaland_spec.py
@@ -7,36 +7,38 @@ class Lavaland_spec:
     num_rows = 10
     num_cols = 10
 
-    # gamma = discount factor
-    # init_prob = |S|*1 vector where each entry denotes the probability of state s being the initial state
-    def __init__(self, gamma, init_prob):
-        self.gamma = gamma
-        self.init_prob = init_prob
+    # # gamma = discount factor
+    # # init_prob = |S|*1 vector where each entry denotes the probability of state s being the initial state
+    # def __init__(self, gamma, init_prob):
+    #     self.gamma = gamma
+    #     self.init_prob = init_prob
 
     # Return the coordinates of a cell's neighbors
-    def get_ngbr_pos_coord(self, row_idx, col_idx):
+    @staticmethod
+    def get_ngbr_pos_coord(row_idx, col_idx):
         ngbr_pos_coords = []
 
         if col_idx > 1:
-            ngbr_pos_coords.append(np.array([row_idx,col_idx-1])) #cell to the right of the current cell
-        if col_idx < self.num_cols:
+            ngbr_pos_coords.append(np.array([row_idx, col_idx-1])) #cell to the right of the current cell
+        if col_idx < Lavaland_spec.num_cols:
             ngbr_pos_coords.append(np.array([row_idx, col_idx+1])) #cell to the left of the current cell
         if row_idx > 1:
-            ngbr_pos_coords.append(np.array([row_idx+1, col_idx])) #cell below the current cell
-        if row_idx < self.num_rows:
-            ngbr_pos_coords.append(np.array([row_idx-1, col_idx])) #cell above the current cell
+            ngbr_pos_coords.append(np.array([row_idx-1, col_idx])) #cell below the current cell
+        if row_idx < Lavaland_spec.num_rows:
+            ngbr_pos_coords.append(np.array([row_idx+1, col_idx])) #cell above the current cell
         return ngbr_pos_coords
 
     # 0 = Up    1 = Down    2 = Left    3 = Right
-    def get_available_action(self, row_idx, col_idx):
+    @staticmethod
+    def get_available_action(row_idx, col_idx):
         available_actions = []
 
-        if col_idx >= 1 & col_idx <= self.num_cols-1:
+        if col_idx >= 1 and col_idx <= Lavaland_spec.num_cols-1:
             available_actions.append(3)
-        if col_idx >= 2 & col_idx <= self.num_cols:
+        if col_idx >= 2 and col_idx <= Lavaland_spec.num_cols:
             available_actions.append(2)
-        if row_idx >= 1 & row_idx <= self.num_rows-1:
+        if row_idx >= 1 and row_idx <= Lavaland_spec.num_rows-1:
             available_actions.append(1)
-        if row_idx >= 2 & row_idx <= self.num_rows:
+        if row_idx >= 2 and row_idx <= Lavaland_spec.num_rows:
             available_actions.append(0)
         return available_actions
