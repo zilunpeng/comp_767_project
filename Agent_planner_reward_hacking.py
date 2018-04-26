6637057cfcd2c79b8baa36731e8f264c702a51ae
Zilun Peng <zilunpeng@gmail.com>
2018-04-25 22:24:48 -0400

    compute z using traj generated from w_true

---
+++ Agent_planner_reward_hacking.py
@@ -0,0 +1,246 @@
+from Lavaland_spec import Lavaland_spec
+import numpy as np
+from scipy.optimize import linprog
+from IRD_reward_hacking import IRD_reward_hacking as IRD
+from baseline import Baseline
+from value_iteration import VI
+
+def get_opposite_action(action):
+    if action==0:
+        return 1
+    elif action==1:
+        return 0
+    elif action==2:
+        return 3
+    elif action==3:
+        return 2
+
+# Return the linear index given a (cell) position-action pair
+def pos_action_pair_2_ind(row_ind, col_ind, action):
+    return (num_rows*(col_ind) + row_ind)*num_actions + action
+
+def sub2ind(row_idx, col_idx):
+    return num_rows * col_idx + row_idx
+
+def ind2sub(ind):
+    return (int(ind % num_cols), int(ind / num_cols))
+
+def form_bounds():
+    bounds = []
+    for i in range(num_cells*num_actions):
+        bounds.append((0, None))
+    bounds.append((None, None))
+    return bounds
+
+def form_ineq_vec():
+    return np.zeros((num_sampled_w, 1))
+
+def form_ineq_mat():
+    linprog_ineq_mat = np.zeros((num_sampled_w, num_cells*num_actions + 1))
+    ind = 0
+    for w in sampled_w:
+        # print(w)
+        w = np.asarray(w).reshape((4,1))
+        cell_type = lavaland.form_testing_rewards(w)
+        rewards = cell_type @ w
+        for r_ind in range(num_rows):
+            for c_ind in range(num_cols):
+                available_actions = lavaland.get_available_action(r_ind, c_ind)
+                for a_ind in range(len(available_actions)):
+                    pa_idx = pos_action_pair_2_ind(r_ind, c_ind, available_actions[a_ind])
+                    ngbr_pos = lavaland.get_ngbr_pos_coord(r_ind, c_ind, available_actions[a_ind])
+                    reward = rewards[ngbr_pos]
+                    linprog_ineq_mat[ind, pa_idx] = -1*reward
+        linprog_ineq_mat[ind, -1] = 1
+        ind = ind + 1
+    initial_state_pos = pos_action_pair_2_ind(5,1,0)
+    linprog_ineq_mat[0:num_sampled_w, initial_state_pos:initial_state_pos+num_actions] = 0
+    return linprog_ineq_mat
+
+def form_eq_vec():
+    linprog_eq_vec = np.zeros((num_cells,1))
+    linprog_eq_vec[sub2ind(5,1)] = 1 #(5,1) is the initial position
+    return linprog_eq_vec
+
+
+# Form the equality matrix (LHS) (|S|+1 * |S||A|+1) to the linear programming solver
+def form_eq_mat():
+    linprog_eq_mat = np.zeros((num_cells, num_cells*num_actions + 1))
+    for r_ind in range(num_rows):
+        for c_ind in range(num_cols):
+            p_idx = sub2ind(r_ind, c_ind)
+            available_actions = lavaland.get_available_action(r_ind, c_ind)
+            for a_ind in range(len(available_actions)):
+                pa_idx = pos_action_pair_2_ind(r_ind, c_ind, available_actions[a_ind])
+                linprog_eq_mat[p_idx, pa_idx] = 1
+
+                ngbr_pos_coord = lavaland.get_ngbr_pos_coord(r_ind, c_ind, available_actions[a_ind])
+                ngbr_pos_coord = ind2sub(ngbr_pos_coord)
+                pa_idx = pos_action_pair_2_ind(ngbr_pos_coord[0], ngbr_pos_coord[1], get_opposite_action(available_actions[a_ind]))
+                linprog_eq_mat[p_idx, pa_idx] = -gamma
+    return linprog_eq_mat
+
+
+'''
+convert a |S|*|S|*|A| vector into policy
+return -1 if there is no action with > 0 probability
+'''
+def convert2policy(x):
+    x = x['x']
+    policy = np.zeros((num_rows, num_cols))
+    for r_ind in range(num_rows):
+        for c_ind in range(num_cols):
+            idx = pos_action_pair_2_ind(r_ind, c_ind, 0)
+            action_prob = x[idx:idx+num_actions]
+            if np.sum(action_prob) == 0:
+                policy[r_ind, c_ind] = -1
+            else:
+                action_prob = np.divide(action_prob, np.sum(action_prob))
+                policy[r_ind, c_ind] = np.argmax(action_prob)
+    return policy
+
+
+def policy_leads_to_lava(lavaland, policy):
+    position = 51
+    # pos_x, pos_y = ind2sub(position)
+    pos_x, pos_y = num_to_coord(position)
+    for _ in range(100): # max traj length = 100
+        action = policy[pos_x][pos_y]
+        if action == -1:
+            action = np.random.randint(4)
+        pos_x, pos_y = lavaland.get_next_state(pos_x, pos_y, action)
+        terrain = lavaland.get_testing_land_type(pos_x, pos_y)
+        if terrain == 3: #hit laba
+            return True
+    return False
+
+def num_to_coord(num):
+    r, c = num // 10, num % 10
+    return (r, c)
+
+
+def coord_To_Num(coord):
+    r, c = coord[0], coord[1]
+    return r * 10 + c  # num from 0 to 99
+
+
+def baseline_policy_leads_to_lava(lavaland, policy):
+    position = 51
+    # pos_x, pos_y = ind2sub(position)
+    pos_x, pos_y = num_to_coord(position)
+    for _ in range(100): # max traj length = 100
+        action = policy[pos_x][pos_y]
+        pos_x, pos_y = lavaland.get_next_state(pos_x, pos_y, action)
+        if pos_x < 0:
+            return False
+        terrain = lavaland.get_testing_land_type(pos_x, pos_y)
+        if terrain == 3: #hit laba
+            return True
+    return False
+
+if __name__ == "__main__":
+
+    # baseline_agent = VI()
+    # baseline_policy = baseline_agent.value_iteration(np.array((-2, 7, 3, 0)))
+    # temp_baseline_policy = np.reshape(baseline_policy, (10, 10))
+    # temp_baseline_policy = np.transpose(temp_baseline_policy)
+    # print("--------baseline policy--------")
+    # print(temp_baseline_policy)
+    # lavaland = Lavaland_spec(10, 10, 4, 4)
+    # hit_lava = baseline_policy_leads_to_lava(lavaland, temp_baseline_policy)
+    # print(hit_lava)
+
+    # sampled_w = [np.array((0.1, -10, 10, 0)),np.array((0.1, -10, 10, -5)), np.array((0.1, -10, 10, 10)),np.array((0.1, -10, 10, -10)),] #just for testing
+    # sampled_w = [np.array((0.1, 0.1, 10, -10))]
+    # sampled_w = [np.array((1, -5, 5, 0))]
+
+    hit_lava_baseline_policy = []
+
+    hit_lava_proxy_w_list = []
+    hit_lava_sampled_w_list = []
+    hit_lava_policy_list = []
+    experiment_num = 100
+
+    w_true = np.random.randint(-10, 10, (50, 4))
+
+    for _ in range(experiment_num):
+        design_weight = np.array(np.random.randint(-10, 10, (1, 4))).flatten()
+        # design_weight = np.array((-2, 7, 3, 0))
+        # design_weight[3] = 0
+
+        print("using proxy weight: ", design_weight)
+        ird = IRD()
+        posterior, true_W = ird.run_ird(design_weight, w_true)
+
+        # sample few candidate true_weight from posterior
+
+        print(true_W)
+        num = true_W.shape[0]
+        # true_W.reshape((num, 4))
+        # sample_space = true_W.tolist()
+        # print(sample_space)
+        num_sampled_w = 10
+        pos = np.divide(posterior, posterior.sum())
+        sampled_w_indices = np.random.choice(num, num_sampled_w, p=pos)
+        sampled_w = true_W[sampled_w_indices].tolist()
+
+        lavaland = Lavaland_spec(10, 10, 4, 4)
+        num_rows = 10
+        num_cols = 10
+        num_cells = num_rows * num_cols
+        num_actions = 4
+        gamma = 0.9
+        linprog_eq_mat = form_eq_mat()
+        linprog_eq_vec = form_eq_vec()
+        linprog_ineq_mat = form_ineq_mat()
+        linprog_ineq_vec = form_ineq_vec()
+
+        c = np.zeros(num_cells*num_actions+1)
+        c[-1] = -1
+
+        bounds = form_bounds()
+
+        x = linprog(c, A_ub=linprog_ineq_mat, b_ub=linprog_ineq_vec, A_eq=linprog_eq_mat, b_eq=linprog_eq_vec, bounds=bounds, options={"disp": True})
+        policy = convert2policy(x)
+        print(policy)
+        # print(x)
+
+        if policy_leads_to_lava(lavaland, policy):
+            hit_lava_proxy_w_list.append(design_weight)
+            hit_lava_sampled_w_list.append(sampled_w)
+            hit_lava_policy_list.append(policy)
+            print("IRD hit lava, [{}]/[{}]".format(len(hit_lava_policy_list), experiment_num))
+
+        # start to run baseline
+        # baseline_agent = Baseline()
+        # baseline_policy = baseline_agent.agent_learn(design_weight)
+        # if policy_leads_to_lava(lavaland, baseline_policy):
+        #     hit_lava_baseline_policy.append(baseline_policy)
+
+        baseline_agent = VI()
+        baseline_policy = baseline_agent.value_iteration(design_weight)
+        temp_baseline_policy = np.reshape(baseline_policy, (10, 10))
+        temp_baseline_policy = np.transpose(temp_baseline_policy)
+        print("--------baseline policy--------")
+        print(temp_baseline_policy)
+        if baseline_policy_leads_to_lava(lavaland, temp_baseline_policy):
+            hit_lava_baseline_policy.append(temp_baseline_policy)
+            print("VI hit lava, [{}]/[{}]".format(len(hit_lava_baseline_policy), experiment_num))
+
+    ratio_hit_traj = len(hit_lava_policy_list)/experiment_num
+    ratio_hit_traj_baseline = len(hit_lava_baseline_policy)/experiment_num
+    print(ratio_hit_traj, ratio_hit_traj_baseline)
+    # print(ratio_hit_traj)
+    print("-------------the end-------------")
+
+    # file = open(“output.txt”, ”w”)
+    # hit_lava_baseline_policy = []
+    # hit_lava_proxy_w_list = []
+    # hit_lava_sampled_w_list = []
+    # hit_lava_policy_list = []
+    #
+    # file.write(hit_lava_baseline_policy)
+    # file.write(hit_lava_sampled_w_list)
+    # file.write(hit_lava_policy_list)
+    # file.write(hit_lava_proxy_w_list)
+    # file.close()
---
+++ IRD_reward_hacking.py
@@ -0,0 +1,209 @@
+import gym
+import gym_lavaland
+import numpy as np
+from Lavaland_spec import Lavaland_spec
+import random
+import copy
+
+
+def sub2ind(row_idx, col_idx):
+    num_rows = 10
+    # return num_rows * row_idx + col_idx
+    return num_rows * col_idx + row_idx
+
+
+def ind2sub(ind):
+    num_cols = 10
+    return (int(ind % num_cols), int(ind / num_cols))
+
+class IRD_reward_hacking:
+
+    def __init__(self):
+        self.num_states = 4
+        self.max_step = 15
+        self.num_traj = 1
+        self.num_proxy_rewards = 1
+        self.beta = 1
+        self.gamma = 0.9
+        self.error = 0.001
+        self.lavaland = Lavaland_spec(10, 10, 4, 4)
+        self.w_true_expected_phi = None
+
+    def run_ird(self, proxy_weight, W_true):
+
+        # h_pos = horizontal position
+        # v_pos = vertical position
+        def sample_action_from_stochastic_policy(policy, h_pos, v_pos):
+            return np.random.choice(4, 1, p=policy[sub2ind(h_pos, v_pos)])
+
+        def update_counters(h_pos, v_pos, land_type, state_freq, land_type_counter):
+            state_freq[sub2ind(h_pos, v_pos)] += 1
+            land_type_counter[land_type] += 1
+            return state_freq, land_type_counter
+
+        # w = proxy reward
+        # max_step = maximum number of steps agent will take if not reaching the terminal
+        # num_traj = number of trajectories that we sample
+        # RETURN:
+        # phi_trajectories: Phi(Epsilon)
+        # path_trajectories: the actual path of each trajectory. A path ends before -1
+        def generate_trajectory_from_policy(env, policy, deterministic):
+            state_freq = np.zeros((100, 1))
+            tot_steps = 0
+            land_type_counter = np.zeros(4)
+            for eps in range(self.num_traj):
+                pos = env.reset()
+                state_freq, land_type_counter = update_counters(pos[0], pos[1], 0, state_freq, land_type_counter)
+                for step in range(self.max_step):
+                    if deterministic:
+                        action = policy[sub2ind(pos[0], pos[1])]
+                    else:
+                        action = sample_action_from_stochastic_policy(policy, pos[0], pos[1])
+                    done, phi_epsilon, pos, _ = env.step(action)
+                    state_freq, land_type_counter = update_counters(pos[0], pos[1], self.lavaland.get_training_land_type(pos[0], pos[1]), state_freq, land_type_counter)
+                    tot_steps += 1
+                    if done:
+                        break
+            # state_freq = np.true_divide(state_freq, self.num_traj)
+            # land_type_counter = np.true_divide(land_type_counter, self.num_traj)
+            return state_freq, land_type_counter
+
+        def calc_Z_approx_bayes_w(expected_Phi, index, w):
+                z_w = 0
+                remaining_phi = np.delete(expected_Phi, index, axis=0)
+                firstTerm = np.dot(w, expected_Phi[index])
+                z_w = z_w + np.exp(firstTerm)
+                rem = [np.exp(self.beta * np.dot(w, phi_i)) for phi_i in remaining_phi]
+                z_w = z_w + sum(rem)
+                return z_w
+
+            #  Return Nx1 vector - state visitation frequencies
+        def compute_state_visition_freq(state_trans_mat, policy, deterministic):
+                N_STATES, _, N_ACTIONS = np.shape(state_trans_mat)
+
+                mu = np.zeros([N_STATES, self.max_step])
+                mu[sub2ind(5, 1), 0] = 1
+                visited_states = [sub2ind(5, 1)]
+
+                for t in range(1, self.max_step):
+                    prev_s = np.where(mu[:, t - 1] > 0)[0]
+                    (prev_s_rind, prev_s_cind) = ind2sub(prev_s)
+                    if deterministic:
+                        s = self.lavaland.get_ngbr_pos_coord(prev_s_rind, prev_s_cind, policy[prev_s])
+                        if s == -1 or prev_s == 85 or s in visited_states:  # terminal or out of bounds or cell has been visited
+                            break
+                        else:
+                            visited_states.append(s)
+                            mu[s, t] += mu[prev_s, t-1]
+                    else:
+                        for a in range(3):
+                            s = self.lavaland.get_ngbr_pos_coord(prev_s_rind, prev_s_cind, a)
+                            visited_states.append(s)
+                            if s == -1 or prev_s == 85 or s in visited_states:
+                                shouldBreak = True
+                            mu[s, t] += mu[prev_s, t-1]*policy[prev_s,a]
+                        if shouldBreak:
+                            break
+                p = np.sum(mu, 1)
+                return p.reshape((N_STATES, 1))
+
+        def value_iteration(state_trans_prob, rewards, deterministic):
+            num_cells = 100  # 10*10 grid
+            num_actions = 4
+            values = np.zeros([num_cells])
+
+            # print(state_trans_prob[0][1])
+            while True:
+                values_tmp = copy.deepcopy(values)
+                for s in range(num_cells):
+                    values[s] = max([sum(
+                        [state_trans_prob[s, s1, a] * (rewards[s] + self.gamma * values_tmp[s1]) for s1 in range(num_cells)])
+                                     for a in range(num_actions)])
+
+                if max([abs(values[s] - values_tmp[s]) for s in range(num_cells)]) < self.error:
+                    break
+
+            if deterministic:
+                # generate deterministic policy
+                policy = np.zeros([num_cells])
+                for s in range(num_cells):
+                    policy[s] = np.argmax([sum([state_trans_prob[s, s1, a] * (rewards[s] + self.gamma * values[s1])
+                                                for s1 in range(num_cells)])
+                                           for a in range(num_actions)])
+
+                return policy
+            else:
+                # generate stochastic policy
+                policy = np.zeros([num_cells, num_actions])
+                for s in range(num_cells):
+                    v_s = np.array(
+                        [sum([state_trans_prob[s, s1, a] * (rewards[s] + self.gamma * values[s1]) for s1 in range(num_cells)]) for a in
+                         range(num_actions)])
+                    policy[s, :] = np.transpose(v_s / np.sum(v_s))
+                return policy
+
+        # run_ird code starts from here
+        env = gym.make('Simple_training_lavaland-v0')
+
+        w = proxy_weight.reshape((self.num_states, 1))
+        cell_type = self.lavaland.form_rewards(w)
+        rewards = cell_type @ w
+        state_trans_prob = self.lavaland.get_state_trans_mat()
+        policy = value_iteration(state_trans_prob, rewards, deterministic=True)
+        temp2 = np.reshape(policy, (10,10))
+        temp2 = np.transpose(temp2)
+        state_freq, land_type_counter = generate_trajectory_from_policy(env, policy, deterministic=True)
+        temp = np.reshape(state_freq, (10,10))
+        temp = np.transpose(temp)
+        expected_telda_phi_w = compute_state_visition_freq(state_trans_prob, policy, deterministic=True)
+        expected_telda_phi_w = np.multiply(state_freq, expected_telda_phi_w)
+        expected_telda_phi_w = np.tile(expected_telda_phi_w, (1, 4))
+        expected_telda_phi_w = np.multiply(cell_type, expected_telda_phi_w)
+        expected_telda_phi_w = np.sum(expected_telda_phi_w, axis=0)
+
+        expected_true_phi = []  # 25 * 4
+        if self.w_true_expected_phi is not None:
+            expected_true_phi = self.w_true_expected_phi
+        else:
+            for w in W_true:
+                w = w.reshape((self.num_states, 1))
+                cell_type = self.lavaland.form_rewards(w)
+                rewards = cell_type @ w
+                state_trans_prob = self.lavaland.get_state_trans_mat()
+                policy = value_iteration(state_trans_prob, rewards, deterministic=True)
+                temp2 = np.reshape(policy, (10, 10))
+                temp2 = np.transpose(temp2)
+                state_freq, land_type_counter = generate_trajectory_from_policy(env, policy, deterministic=True)
+                expected_true_phi_w = compute_state_visition_freq(state_trans_prob, policy, deterministic=True)
+                temp = np.reshape(expected_true_phi_w, (10, 10))
+                temp = np.transpose(temp)
+                expected_true_phi_w = np.multiply(state_freq, expected_true_phi_w)
+                expected_true_phi_w = np.tile(expected_true_phi_w, (1, 4))
+                expected_true_phi_w = np.multiply(cell_type, expected_true_phi_w)
+                expected_true_phi_w = np.sum(expected_true_phi_w, axis=0)
+                expected_true_phi.append(expected_true_phi_w)
+            self.w_true_expected_phi = expected_true_phi
+
+        # calculate posterior for each possible true_w:
+        # input w_telda 1*4, output posterior 1 * 25
+        posteriors = []
+        store_numerators = []
+        store_z = []
+        for idx, w_true in enumerate(W_true):
+            expected_true_reward = np.dot(expected_telda_phi_w, w_true)
+            numerator = np.exp(self.beta * expected_true_reward)
+            z_w_true = calc_Z_approx_bayes_w(expected_true_phi, idx, w_true)
+            store_numerators.append(numerator)
+            store_z.append(z_w_true)
+            likelihood = np.true_divide(numerator, z_w_true)
+            post = likelihood
+            posteriors.append(post)
+        posteriors = np.asarray(posteriors).flatten()
+
+        print(posteriors)
+        print(posteriors.sum())
+        print(np.divide(posteriors, posteriors.sum()))
+        print(posteriors.max())
+
+        return posteriors, W_true
+
